a6455b10dc47c9b5086e35e06a2b46b35623636c
Artyom Zinin <NicYKN@yandex.ru>
2023-03-11 01:17:12 +0300

    2-module tasks: done

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (const key in salaries) {
+    if (typeof(salaries[key]) === "number" && Boolean(parseInt(salaries[key]))) {
+      sum = sum + salaries[key];
+    }
+  }
+  return sum;
 }
--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,11 @@
 function isEmpty(obj) {
-  // ваш код...
+  let count = 0;
+  for (let key in obj) {
+    [].push(key).length;
+    count++;
+    break;
+  }
+  return !Boolean(count);
 }
+
+// или через Object.keys(object).length;
