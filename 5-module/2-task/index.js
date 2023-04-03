aee4c9e91fe103ab0221ab1d0f7a1bb37c5ebe84
Artyom Zinin <NicYKN@yandex.ru>
2023-04-03 19:36:03 +0300

    5 module: tasks done

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,12 @@
 function toggleText() {
-  // ваш код...
+  const toggleTextButton = document.querySelector('.toggle-text-button');
+  const divText = document.querySelector('#text');
+
+  toggleTextButton.addEventListener('click', () => {
+    if (divText.hasAttribute("hidden")) {
+      divText.removeAttribute("hidden");
+    } else {
+      divText.setAttribute("hidden", "hidden");
+    }
+  });
 }
